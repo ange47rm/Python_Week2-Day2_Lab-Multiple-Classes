ca94b699ec62d375330f60da59f5917344eaffca
ange47rm <ange47rm@hotmail.com>
2020-11-24 15:24:37 +0000

    Completed all tests.

--- bus_lab_start_code/src/person.py
+++ bus_lab_start_code/src/person.py
@@ -1,5 +1,10 @@
 class Person():
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, destination):
         self.name = name
         self.age = age
+        self.destination = destination
+
+    def person_gets_on_bus (self, bus):
+        if self.destination == bus.destination:
+            return True
--- bus_lab_start_code/tests/person_test.py
+++ bus_lab_start_code/tests/person_test.py
@@ -1,9 +1,10 @@
 import unittest
 from src.person import Person
+from src.bus import Bus
 
 class TestPerson(unittest.TestCase):
     def setUp(self):
-        self.person = Person("Guido van Rossum", 64)
+        self.person = Person("Guido van Rossum", 64, "Sun")
 
     # @unittest.skip("Delete this line to run the test")
     def test_person_has_name(self):
@@ -12,3 +13,12 @@ class TestPerson(unittest.TestCase):
     # @unittest.skip("Delete this line to run the test")
     def test_person_has_age(self):
         self.assertEqual(64, self.person.age)
+
+
+    # @unittest.skip("Delete this line to run the test")
+    def test_person_gets_on_bus(self):
+        person1 = Person("Skye", 33, "SilverLane")
+        person2 = Person("David", 86, "GreenTown")
+        bus = Bus(88, "SilverLane")
+        self.assertEqual(True, person1.person_gets_on_bus(bus))
+        #self.assertEqual(False, person2.person_gets_on_bus(bus))
